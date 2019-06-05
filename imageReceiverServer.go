81db682f6ab822ec8df18511a9f0b13b6e68de16
raz.shenkman <shenkmanraz@gmail.com>
2019-06-05 15:49:30 +0300

    §liz ze bishvilech

--- imageReceiverServer.go
+++ imageReceiverServer.go
@@ -4,13 +4,13 @@ import "net"
 import "fmt"
 import "bufio"
 import "strings" // only needed below for sample processing
+import "os"
 
 const (
-    CONN_HOST = "127.0.0.1"
-    CONN_PORT = "3333"
-    CONN_TYPE = "tcp"
+	CONN_HOST = "127.0.0.1"
+	CONN_PORT = "8181"
+	CONN_TYPE = "tcp"
 )
-
 
 func main() {
 	fmt.Println("Launching server...")
@@ -21,7 +21,7 @@ func main() {
 		os.Exit(1)
 	}
 	// Close the listener when the application closes.
-	defer l.Close()
+	defer ln.Close()
 	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
 	// accept connection on port
 	conn, err := ln.Accept()
@@ -32,16 +32,16 @@ func main() {
 	}
 }
 
-func handleClient(conn *TCPConn) {
+func handleClient(conn net.Conn) {
 	// run loop forever (or until ctrl-c)
 	for {
-	// will listen for message to process ending in newline (\n)
-	message, _ := bufio.NewReader(conn).ReadString('\n')
-	// output message received
-	fmt.Print("Message Received:", string(message))
-	// sample process for string received
-	newmessage := strings.ToUpper(message)
-	// send new string back to client
-	conn.Write([]byte(newmessage + "\n"))
+		// will listen for message to process ending in newline (\n)
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		// output message received
+		fmt.Print("Message Received:", string(message))
+		// sample process for string received
+		newmessage := strings.ToUpper(message)
+		// send new string back to client
+		conn.Write([]byte(newmessage + "\n"))
 	}
 }
--- mockClient.go
+++ mockClient.go
@@ -7,17 +7,38 @@ import "os"
 
 func main() {
 
-  // connect to this socket
-  conn, _ := net.Dial("tcp", "127.0.0.1:8081")
-  for { 
-    // read in input from stdin
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Text to send: ")
-    text, _ := reader.ReadString('\n')
-    // send to socket
-    fmt.Fprintf(conn, text + "\n")
-    // listen for reply
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Print("Message from server: "+message)
-  }
+	// connect to this socket
+	conn, err := net.Dial("tcp", "127.0.0.1:8181")
+	if err != nil {
+		fmt.Println("Error connecting to server:", err.Error())
+		os.Exit(1)
+	}
+	for {
+		// read in input from stdin
+		reader := bufio.NewReader(os.Stdin)
+
+		usrName, _ := reader.ReadString('\n')
+		if usrName == "q\n" {
+			os.Exit(0)
+		}
+		// send to socket
+		fmt.Fprintf(conn, strlen(usrName)+"\n")
+		fmt.Fprintf(conn, usrName+"\n")
+		password, _ := reader.ReadString('\n')
+		if password == "q\n" {
+			os.Exit(0)
+		}
+		// send to socket
+		fmt.Fprintf(conn, strlen(password)+"\n")
+		fmt.Fprintf(conn, password+"\n")
+		fmt.Print("Please insert path of image to send to HealthySkin server: " +
+			"\n(to quit insert 'q')\n")
+		password, _ := reader.ReadString('\n')
+		if password == "q\n" {
+			os.Exit(0)
+		}
+		// listen for reply
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+	}
 }
